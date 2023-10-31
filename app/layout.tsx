1a899175914b2722667aa8fa6d5cfa64a8b103d6
Ivan Khurbatau <hurbatushka@gmail.com>
2023-10-31 20:22:51 +0300

    Создана миграция в MySQL изменен шрифт

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 
 import NavBar from './components/NavBar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Issue Tracker',
@@ -16,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        <main>{children}</main>
+        <main className="flex justify-center">{children}</main>
       </body>
     </html>
   );
