993386ea375dd900308348c49d9aa91d7f08f3c8
Ivan Khurbatau <hurbatushka@gmail.com>
2023-10-31 22:13:42 +0300

    отправка формы /new

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,18 +4,38 @@ import { TextField, Button } from '@radix-ui/themes';
 import { MdTitle } from 'react-icons/md';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 export default function NewIssue() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues/', data);
+        router.push('/issues');
+      })}>
       <TextField.Root>
         <TextField.Slot>
           <MdTitle height="16" width="16" />
         </TextField.Slot>
-        <TextField.Input size="3" placeholder="Тема обращения..." />
+        <TextField.Input size="3" placeholder="Тема обращения..." {...register('title')} />
       </TextField.Root>
-      <SimpleMDE placeholder="Опишите Вашу проблему..." />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Опишите Вашу проблему..." {...field} />
+        )}></Controller>
       <Button>Отправить</Button>
-    </div>
+    </form>
   );
 }
