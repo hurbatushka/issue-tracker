1686a0fc54b017fde3575af882c0e3665a01d9e8
Иван Хурбатов <hurbatushka@gmail.com>
2023-11-01 08:40:52 +0300

    Обработка ошибок при отправке

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 import prisma from '@/prisma/client';
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Тема отсутствует').max(255, 'Слишком длинная тема'),
+  description: z.string().min(1, 'Описание отсутствует'),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const valid = createIssueSchema.safeParse(body);
   if (!valid.success) {
-    return NextResponse.json(valid.error.errors, { status: 400 });
+    return NextResponse.json(valid.error.format(), { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 'use client';
-import React from 'react';
-import { TextField, Button } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import { TextField, Button, Callout } from '@radix-ui/themes';
 import { MdTitle } from 'react-icons/md';
+import { BiErrorCircle } from 'react-icons/bi';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
@@ -16,26 +17,41 @@ interface IssueForm {
 export default function NewIssue() {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues/', data);
-        router.push('/issues');
-      })}>
-      <TextField.Root>
-        <TextField.Slot>
-          <MdTitle height="16" width="16" />
-        </TextField.Slot>
-        <TextField.Input size="3" placeholder="Тема обращения..." {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Опишите Вашу проблему..." {...field} />
-        )}></Controller>
-      <Button>Отправить</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-10">
+          <Callout.Icon>
+            <BiErrorCircle />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues/', data);
+            router.push('/issues');
+          } catch (error) {
+            setError('Произошла непредвиденная ошибка');
+          }
+        })}>
+        <TextField.Root>
+          <TextField.Slot>
+            <MdTitle height="16" width="16" />
+          </TextField.Slot>
+          <TextField.Input size="3" placeholder="Тема обращения..." {...register('title')} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Опишите Вашу проблему..." {...field} />
+          )}></Controller>
+        <Button>Отправить</Button>
+      </form>
+    </div>
   );
 }
